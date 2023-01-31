5431be8527bbe228ec21b6a5aae7eed091f13982
Christober <christober2907@gmail.com>
2023-01-31 18:33:07 +0530

    Example for generics

--- src/Main.java
+++ src/Main.java
@@ -1,38 +1,23 @@
-import java.awt.Color;
-import java.awt.Font;
-
-import javax.swing.BorderFactory;
-import javax.swing.ImageIcon;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.border.Border;
-
 public class Main{
-	public static void main(String [] args) {
+	public static void main(String[] args) {
 		
-		JLabel label = new JLabel(); // create label
-		Border border = BorderFactory.createLineBorder(Color.green,3 );
-		label.setText("Hey! chris"); //set text of label
-		ImageIcon image = new ImageIcon("c logo.jpg");
-		label.setIcon(image);
-		label.setHorizontalTextPosition(JLabel.CENTER); // set text LEFT,CENTRE,RIGHT OF imageicon
-		label.setVerticalTextPosition(JLabel.TOP);      // set txt top, centre , bottom of imageicon
-		label.setForeground(new Color(0x123456));      // text color
-		label.setFont(new Font("MV Boli",Font.PLAIN, 20)); // set font of text
-		label.setIconTextGap(-20); // set gap of  text to image
-		label.setBackground(Color.black); // set background color
-		label.setOpaque(true); // display background color
-		label.setBorder(border); // sets border of label 
-		label.setVerticalAlignment(JLabel.CENTER); // set vertical position of text within label
-		label.setHorizontalAlignment(JLabel.CENTER); // set horizontal position of text within label
+		
+		Integer[] intArray = {1, 2, 3, 4, 5, 6};
+		Double[] doubleArray = {4.4, 8.5, 9.0, 4.1, 3.17};
 	
-		JFrame frame = new JFrame();
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setSize(450,450);
-		frame.setVisible(true);
+		Character[] charArray = {'c', 'h', 'r', 'i', 's'};
+		String[] stringArray = {"H", "E", "L", "L", "O"};
 		
-		frame.add(label);
+		displayArray(intArray);
+		displayArray(doubleArray);
+		displayArray(charArray);
+		displayArray(stringArray);
 		
-		frame.pack();
+	}
+	public static <T> void displayArray(T[] array) {
+		for (T x : array) {
+			System.out.print(x+" ");
+			}
+		System.out.println();
 	}
 }
