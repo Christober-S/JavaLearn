683b43c773148ba9d91013a9d36d28bce9c75a3b
Christober <christober2907@gmail.com>
2023-01-31 18:40:24 +0530

    Example for return in generics

--- src/Main.java
+++ src/Main.java
@@ -8,16 +8,27 @@ public class Main{
 		Character[] charArray = {'c', 'h', 'r', 'i', 's'};
 		String[] stringArray = {"H", "E", "L", "L", "O"};
 		
-		displayArray(intArray);
-		displayArray(doubleArray);
-		displayArray(charArray);
-		displayArray(stringArray);
+		//displayArray(intArray);
+		//displayArray(doubleArray);
+		//displayArray(charArray);
+		//displayArray(stringArray);
+		
+		System.out.println(getFirst(intArray));
+		System.out.println(getFirst(doubleArray));
+		System.out.println(getFirst(charArray));
+		System.out.println(getFirst(stringArray));
 		
 	}
-	public static <T> void displayArray(T[] array) {
-		for (T x : array) {
-			System.out.print(x+" ");
-			}
-		System.out.println();
+	//public static <T> void displayArray(T[] array) {
+	//	for (T x : array) {
+	//		System.out.print(x+" ");
+	//		}
+	//	System.out.println();
+	//}
+	
+	public static <T> T getFirst(T[] array) {
+			
+		return array[0];
 	}
+	
 }
